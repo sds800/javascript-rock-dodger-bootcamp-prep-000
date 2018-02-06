5540e3304f857e655a073639ee295b6b51376329
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 15:37:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ function moveDodgerRight() {
    var leftNumbers = dodger.style.left.replace('px', '')
    var left = parseInt(leftNumbers, 10)
 
-   if (left < 400) {
+   if (left < 360) {
      dodger.style.left = `${left + 4}px`
    }
  }
