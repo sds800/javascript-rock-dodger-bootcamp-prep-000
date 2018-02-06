77e937c3223d85542d6e739744ea22401f9095b1
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 22:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = dodgerLeftEdge + 40
+    const dodgerRightEdge = (dodgerLeftEdge + 40)
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = rockLeftEdge + 20
+    const rockRightEdge = (rockLeftEdge + 20)
 
     if (
       (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
@@ -60,6 +60,38 @@ function checkCollision(rock) {
       //          *    and the rock's right edge is > the DODGER's right edge
               //  */
 
+              function checkCollision(rock) {
+                const rockTop = positionToInteger(rock.style.top);
+                // rocks are 20px high
+                // DODGER is 20px high
+                // GAME_HEIGHT - 20 - 20 = 360px;
+                if (rockTop > 360) {
+                  const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+
+                  // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+                  const dodgerRightEdge = (dodgerLeftEdge + 40);
+
+                  const rockLeftEdge = positionToInteger(rock.style.left);
+
+                  // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+                  const rockRightEdge = (rockLeftEdge + 20);
+
+                  if (false === /*** Think about it -- what's happening here?
+                             * There's been a collision if one of three things is true:
+                             * 1. The rock's left edge is < the DODGER's left edge,
+                             *    and the rock's right edge is > the DODGER's left edge;
+                             * 2. The rock's left edge is > the DODGER's left edge,
+                             *    and the rock's right edge is < the DODGER's right edge;
+                             * 3. The rock's left edge is < the DODGER's right edge,
+                             *    and the rock's right edge is > the DODGER's right edge */
+              (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgetLeftEdge) ||
+              (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
+              (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)
+                           ) {
+                    return true;
+                  }
+                }
+              }
 
 
 function createRock(x) {
