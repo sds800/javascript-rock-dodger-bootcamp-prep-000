5a4b6c9839e1bdfd146e89fb3e69657f1dc4f05e
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 21:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -173,14 +173,21 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
 
-  if (left > 0) {
-    // window.requestAnimationFrame(moveDodgerLeft)
-    dodger.style.left = `${left - 4}px`
-  }
-  // window.requestAnimationFrame(moveDodgerLeft)
+  // if (left > 0) {
+  //
+  //   dodger.style.left = `${left - 4}px`
+  // }
+  window.requestAnimationFrame(function(){
+    if (left > 0) {
+
+      dodger.style.left = `${left - 4}px`
+    }
+
+  })
 }
 // window.requestAnimationFrame(moveDodgerLeft)
 // document.addEventListener('keydown', function(e) {
