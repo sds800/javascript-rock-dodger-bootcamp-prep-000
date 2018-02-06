a31f2785083842647451627041a5cad4c03ff726
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 20:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ function moveDodgerRight() {
    var leftNumbers = dodger.style.left.replace('px', '')
    var left = parseInt(leftNumbers, 10)
 
-   if (left < 360) {
+   if (left < 359) {
      dodger.style.left = `${left + 4}px`
    }
    window.requestAnimationFrame(moveDodgerRight)
