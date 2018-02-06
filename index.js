b3424c3b025da552545ff48cdea17e76400b58ff
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 20:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -166,12 +166,12 @@ function moveDodgerLeft() {
    */
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
-
+window.requestAnimationFrame(moveDodgerLeft)
   if (left > 4) {
 
     dodger.style.left = `${left - 4}px`
   }
-  window.requestAnimationFrame(moveDodgerLeft)
+  // window.requestAnimationFrame(moveDodgerLeft)
 }
 // window.requestAnimationFrame(moveDodgerLeft)
 // document.addEventListener('keydown', function(e) {
