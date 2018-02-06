d63f29f3897ca91264adb208378c906a35b8b091
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 19:02:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function moveRock() {
     //  * If a rock collides with the DODGER,
     //  * we should call endGame()
     //  */
-     if (checkCollision(rock) == true) {
+     if (checkCollision(rock) /*== true*/) {
       return endGame()
      }
     //  * Otherwise, if the rock hasn't reached the bottom of
