e68f7739551285442558efdd29ef3aa4c3856554
SDS 800 <jeremyhyman@gmail.com>
2018-02-06 22:35:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = (dodgerLeftEdge + 40)
+    const dodgerRightEdge = (dodgerLeftEdge + 40);
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = (rockLeftEdge + 20)
+    const rockRightEdge = (rockLeftEdge + 20);
 
     if (
       (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
@@ -49,7 +49,7 @@ function checkCollision(rock) {
        }
     }
 }
-  
+
 
 function createRock(x) {
   const rock = document.createElement('div')
